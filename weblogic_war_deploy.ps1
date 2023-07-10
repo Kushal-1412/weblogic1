92b63eec3214a74ce604659c401fd9ec57f5874f
Kushal-1412 <110546068+Kushal-1412@users.noreply.github.com>
2023-07-10 14:36:44 +0530

    Add files via upload

---
+++ weblogic_war_deploy.ps1
@@ -0,0 +1,19 @@
+# Set the variables for the deployement
+#USE t3 INSTEAD OF http IF YOU HAVE NOT ENABLED THE TUNNELING. IF YOU WANT TO ENABLE TUNNELING IN SERVER>PROTOCALS>GENERAL>ENABLE TUNNELING
+$adminUrl = "t3://localhost:7001"
+$username = "weblogic"
+$password = "weblogic@123"
+$appPath = "D:\downloads\sample.war"
+$target = "AdminServer"
+
+# Deploy the application using weblogic.Deployer
+#"YOU CAN FIND WEBLOGIC.JAR IN GIVEN PATH:- Oracle_Home\wlserver\server\lib\weblogic.jar (WEBLOGIC.DEPLOYER IS PRESENT IN THE WEBLOGIC.JAR AS A CLASSPATH(CP))"
+java -cp "D:\Weblogic\Installation\wlserver\server\lib\weblogic.jar" weblogic.Deployer `
+ -verbose `
+ -noexit `
+ -adminurl $adminUrl `
+ -username $username `
+ -password $password `
+ -deploy $appPath `
+ -targets $target `
+ -name sample
